43d96bead65805529dfe0ef38ef4f27273162d41
Deepanshu <deepanshukhanna35@example.com>
2024-06-08 21:48:48 +0530

    First deployement

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http= require('http');
 // console.log(http);
+
+const {logResponse,logResponse1}=require('./util')
 http.createServer(function(req,res){
     // res.end('Testing our server');
+    logResponse();
+    logResponse1();
     res.end('First deployment');
 
 }).listen(5005,function(){
---
+++ util.js
@@ -0,0 +1,7 @@
+const logResponse = ()=>{
+    console.log('Hello Deepanshu');
+}
+const logResponse1 = ()=>{
+    console.log('How r u');
+}
+module.exports = {logResponse,logResponse1};
