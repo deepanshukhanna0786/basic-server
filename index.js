31e7a74f60a1d50ddf65095c50a7075f7fcdd525
Deepanshu <deepanshukhanna35@example.com>
2024-06-08 20:42:49 +0530

    First deployement

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http= require('http');
 // console.log(http);
 http.createServer(function(req,res){
-    res.end('Testing our server');
+    // res.end('Testing our server');
     res.end('First deployment');
 
-}).listen(5001,function(){
+}).listen(5005,function(){
     console.log('I am running')
 })
